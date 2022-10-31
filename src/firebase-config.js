8a4adcf4b26dd67d7a33ac9a0cbf0cf7e0855d54
Andresdormir <84159581+Andresdormir@users.noreply.github.com>
2022-10-31 01:14:16 -0600

    getuser not found

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from '@firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyCdW5FE604dcQCPSnKWNFv9OWmACMhYk4I",
+    authDomain: "crud-1a1f8.firebaseapp.com",
+    projectId: "crud-1a1f8",
+    storageBucket: "crud-1a1f8.appspot.com",
+    messagingSenderId: "1054066617231",
+    appId: "1:1054066617231:web:e1db0e3cfe472aaaa239fa"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+
+  export const db = getfirestore(app);
