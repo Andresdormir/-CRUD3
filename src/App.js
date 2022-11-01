1b5c16dd53e07c9d2ae916eed959e53bf72d6e50
EDev <eduardolmedi00@gmail.com>
2022-10-31 18:05:39 -0600

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState,useEffect} from "react"; 
+import {useState,useEffect, useCallback} from "react"; 
 import './App.css';
 import {db} from './firebase-config'
 import {collection,getDocs} from 'firebase/firestore';
@@ -6,15 +6,20 @@ function App() {
   const [users,setUsers] = useState([]);
   const usersCollectionRef = collection (db, "users");
   
-  const getUsers = async () => {
-    const data = await getDocs (usersCollectionRef);
-    setUsers(data.docs.map((doc) => ({... doc.data(),id: doc.id})));
-   };
+  const getUsers = useCallback(
+    async () => {
+      const data = await getDocs (usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({... doc.data(),id: doc.id})));
+    },
+    [usersCollectionRef],
+  )
+  
+
   
   useEffect(() => {
      getUsers();
-  },[]);
-  
+  },[getUsers]);
+
   return (
     <div className="App"> {users.map((user)=>{return <div><h1>Name:{user.name}</h1><h1>Age:{user.age}</h1></div>;})}</div>
   );
